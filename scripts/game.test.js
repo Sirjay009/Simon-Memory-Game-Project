9f4363fbbc89e758665ea911dbe4dbc8eef5373d
Chima Obutte <obuttechima@rocketmail.com>
2024-11-25 06:02:20 +0000

    Create fail test to check if score key exists in game object

--- scripts/game.test.js
+++ scripts/game.test.js
@@ -1,7 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const { expect } = require("@jest/globals");
+const { default: test } = require("node:test");
+const { describe } = require("yargs");
+
 beforeEach(() => {
     let fs = require("fs");
     let fileContents = fs.readFileSync("index.html", "tf-8");
     document.open();
     document.write(fileContents);
     document.close();
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
 });
